bd626d763ab518399aacd181f50ccaa3b1b742bc
Eugene_Korzh <korzh367@gmail.com>
2026-02-18 23:01:02 +0100

    first commit

---
+++ src/models/note.js
@@ -0,0 +1,44 @@
+import { model, Schema } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
+
+const notesSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      default: '',
+      trim: true,
+    },
+    tag: {
+      type: String,
+      enum: TAGS,
+      default: 'Todo',
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+notesSchema.index(
+  {
+    title: 'text',
+    content: 'text',
+  },
+  {
+    name: 'NotesTextIndex',
+    weights: {
+      title: 10,
+      content: 2,
+    },
+    default_language: 'english',
+  },
+);
+
+export const Note = model('Note', notesSchema);
