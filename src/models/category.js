ee099fd8decb7d4d80f176b9b44ad2ee91497608
Eugene_Korzh <korzh367@gmail.com>
2026-02-19 22:40:30 +0100

    feat: create models

---
+++ src/models/category.js
@@ -0,0 +1,16 @@
+import { model, Schema } from 'mongoose';
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+const Category = model('categories', categorySchema);
---
+++ src/models/user.js
@@ -0,0 +1,28 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    avatarUrl: {
+      type: String,
+      default: '',
+    },
+    articlesAmount: {
+      type: Number,
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+export const User = model('users', userSchema);
