b8363793dc3ef89bcebef3573434f974a6feb210
Eugene_Korzh <korzh367@gmail.com>
2026-02-19 21:27:46 +0100

    feat: create server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { connectMongoDB } from './db/connectMongoDB.js';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
 app.use(express.json());
 app.use(cors());
 
