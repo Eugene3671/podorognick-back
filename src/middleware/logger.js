469744123cd0768c2531c60b98217514de069022
Vitalii_gh <a.g.vitalii2022@gmail.com>
2026-02-20 19:29:34 +0200

    enable logger in dev mode only

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -2,17 +2,21 @@
 
 import pino from 'pino-http';
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 export const logger = pino({
-  level: 'info',
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'HH:MM:ss',
-      ignore: 'pid,hostname',
-      messageFormat:
-        '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-      hideObject: true,
-    },
-  },
+  level: isDevelopment ? 'debug' : 'info',
+  transport: isDevelopment
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'HH:MM:ss',
+          ignore: 'pid,hostname',
+          messageFormat:
+            '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+          hideObject: true,
+        },
+      }
+    : undefined,
 });
