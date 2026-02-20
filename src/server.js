b70fae02d6623f8b762f295d2d842ea5955fd621
Vitalii_gh <a.g.vitalii2022@gmail.com>
2026-02-20 18:32:43 +0200

    add feat-s solo get for userId and get from all users

--- src/server.js
+++ src/server.js
@@ -1,15 +1,33 @@
-import 'dotenv/config';
+// src/server.js
+
 import express from 'express';
+import 'dotenv/config';
 import cors from 'cors';
+
 import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import usersRoutes from './routes/usersRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+// глобальні middleware
+app.use(logger);
 app.use(express.json());
 app.use(cors());
 
+// підключаємо групу маршрутів юзерів
+app.use('/api/users', usersRoutes);
+
+// 404 і обробник помилок — наприкінці ланцюжка
+app.use(notFoundHandler);
+app.use(errorHandler);
+
 await connectMongoDB();
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
