87813e180282f7da6dcea4f42e2b22b6f5751992
Maksym Zhelezniak <zzzeleznyakmaxim@gmail.com>
2026-02-24 16:11:28 +0200

    fix cors

--- src/server.js
+++ src/server.js
@@ -19,10 +19,27 @@ import rateLimit from 'express-rate-limit';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://podorognick-front.vercel.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
+
 // глобальні middleware
 app.use(logger);
 app.use(express.json({ limit: '100kb' }));
-app.use(cors());
+
 app.use(cookieParser());
 app.use(helmet());
 app.use(
